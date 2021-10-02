3b67b1deb1e64ff034c81ae38ac7db8cfef587a7
ExposedCat <artem13.prokop@gmail.com>
2021-10-02 22:55:50 +0300

    Add JSDoc documentation

--- xquery.js
+++ xquery.js
@@ -3,12 +3,20 @@ class XQuery {
         this.elements = document.querySelectorAll(selector)
     }
 
+    /**
+     * Execute function for each selected element
+     * @param {(HTMLElement?) => void} func - function to be executed
+     */
     do(func) {
         for (const element of this.elements) {
             func(element)
         }
     }
 
+    /**
+     * Get selected HTML element(s)
+     * @returns {HTMLElement|HTMLElement[]}
+     */
     unwrap() {
         if (this.elements.length === 1) {
             return this.elements[0]
@@ -18,8 +26,20 @@ class XQuery {
     }
 }
 
-const $ = (...args) => new XQuery(...args)
+/**
+ * Create new XQuery instance
+ * @param {string} selector - selector
+ * @returns {XQuery}
+ */
+const $ = selector => new XQuery(selector)
 
+/**
+ * Create new XQuery instance
+ * @param {string} event - event name
+ * @param {string} parentSelector - selector of parent element
+ * @param {string} selector - selector
+ * @param {(event: Event) => void} func - function to be executed
+ */
 function handleEvent(event, parentSelector, selector, func) {
     const parent = document.querySelector(parentSelector)
     parent.addEventListener(event, event => {
